7c4066764c1e68b303f3fa5c63d21aea7d9bdcbd
unknown <user@icsd-androtomist.aegean.gr>
2020-05-25 11:59:06 +0300

    IProcessor

---
+++ App/Models/Processing/Terminal/TerminalParse/IntentParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using Androtomist.Models.Database;
+using Androtomist.Models.Database.Entities;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Androtomist.Models.Processing
+{
+    public class IntentParser
+    {
+        /// <summary>
+        /// Converts the text output from terminal to json with permissions, may need adjustments. I din't have time to write an xml parsing function.
+        /// TODO: Change this silly function with a function that parses XML and reads the names of the intents
+        /// </summary>
+        public string ParseIntentJson(string intentOutput)
+        {
+
+            int ifIndex = intentOutput.IndexOf("intent-filter");
+            intentOutput = intentOutput.Substring(ifIndex);
+            intentOutput = intentOutput.Replace("\"", "");
+            intentOutput = intentOutput.Replace("(", "[");
+            intentOutput = intentOutput.Replace(")", "]");
+
+            MatchCollection matches = Regex.Matches(intentOutput, @"(\[(?:\[??[^\[]*?\]))");
+
+            string json = "";
+            foreach (Match match in matches)
+            {
+                foreach (Capture capture in match.Captures)
+                {
+                    if (capture.ToString().Contains("Raw"))
+                        json += capture.ToString();
+                }
+            }
+
+            json = json.Replace("][", "],[");
+            json = json.Replace(":", ":\"");
+            json = json.Replace("]", "\"");
+            json = json.Replace("[", "");
+            json = json.Replace("Raw:", "");
+            json = json.Replace(" ", "");
+            json = "{\"intent\":[" + json + "]}";
+
+            return json;
+        }
+    }
+}
--- App/Models/Processing/Trainer.cs
+++ App/Models/Processing/Trainer.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Data;
-using System.Collections.Generic;
-using Androtomist.Models.Database;
 using Androtomist.Models.Database.Entities;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Androtomist.Models.Results;
-using Newtonsoft.Json;
 
 namespace Androtomist.Models.Processing
 {
-    public class Trainer : DBClass
+    public class Trainer : IProcessor
     {
         private Terminal t1;
         private File file;
@@ -41,69 +35,15 @@ namespace Androtomist.Models.Processing
         {
             foreach (DataRow row in sampleDt.Rows)
             {
-                t1 = new Terminal(true);
-
                 if (!long.TryParse(row["FILE_ID"].ToString(), out long FID))
                     throw new Exception("Error getting files!");
 
                 file = new File(FID);
-
-                ConnectDevice();
-                InstallSample();
-                Analyze();
-                DisconnectDevice();
-                t1.dispose();
+              
+                Analyze();                               
             }
 
             isSuccesful = true;
-        }
-
-        /// <summary>
-        /// Connects VM with ADB and Frida
-        /// </summary>
-        private void ConnectDevice()
-        {
-            if (file.ExistsProp)
-            {
-                _ = t1.exec("cd " + Info.TOOLS_PATH + " && adb.exe connect " + Info.REMOTE_ADDR);
-                _ = t1.exec("cd " + Info.TOOLS_PATH + " && adb.exe push ../frida-server-12.6.23-android-x86_64ata/local/tmp");
-                _ = t1.exec("cd " + Info.TOOLS_PATH + " && adb.exe shell \"su -c 'chmod 755ata/local/tmp/frida-server-12.6.23-android-x86_64'\";");
-                _ = t1.exec("cd " + Info.TOOLS_PATH + " && adb.exe shell \"su -c '/data/local/tmp/frida-server-12.6.23-android-x86_64 >/dev/null 2>&1 &'\";");
-            }
-            else
-                throw new Exception("File does not exists");
-
-            preperationSuccesful = true;
-        }
-
-        /// <summary>
-        /// Disconnects the device
-        /// </summary>
-        private void DisconnectDevice()
-        {
-            if (file.ExistsProp) 
-                t1.exec("cd " + Info.TOOLS_PATH + " && adb.exe disconnect " + Info.REMOTE_ADDR);
-            else
-                throw new Exception("File does not exists");
-
-            preperationSuccesful = true;
-        }
-
-
-        /// <summary>
-        /// Installs sample to VM
-        /// </summary>
-        private void InstallSample()
-        {
-            if (file.ExistsProp)
-            {
-                _ = t1.exec("cd " + Info.TOOLS_PATH + " && adb.exe push '" + Info.PROJECT_PATH + "" + file.RELATIVE_PATH + "'ata/local/tmp;");
-                _ = t1.exec("cd " + Info.TOOLS_PATH + " && adb.exe shell 'cdata/local/tmp; pm install " + file.ORIGINAL_FILE_NAME.Replace("(", "\\(").Replace(")","\\)") + "';");
-            }
-            else
-                throw new Exception("File does not exists");
-
-            installSuccesful = true;
         }
 
         /// <summary>
@@ -113,13 +53,20 @@ namespace Androtomist.Models.Processing
         {
             if (file.ExistsProp)
             {
-                //Analysis steps
+                //static analysis
                 ExtractPackageName();
                 ExtractPermissions();
                 ExtractIntent();
-                Decompile();
-                GetApiCalls();
+                //Decompile();
+                //GetApiCalls();
+
+                //dynamic analysis
+                t1 = new Terminal();
+                ConnectDevice();
+                InstallSample();
                 ExtractDynamicData();
+                DisconnectDevice();
+                t1.dispose();
             }
             else
                 throw new Exception("File does not exists");
@@ -127,237 +74,5 @@ namespace Androtomist.Models.Processing
             analysisSuccesful = true;
         }
 
-        /// <summary>
-        /// Reads package name
-        /// </summary>
-        private void ExtractPackageName()
-        {
-            string package = string.Join("", t1.cmd("cd " + Info.TOOLS_PATH + " && aapt.exe  dump badging " + file.FILE_PATH + ""));
-
-            int pack_start = package.IndexOf("package: name='");
-            string package_name = package.Substring(pack_start + 15);
-            package_name = package_name.Substring(0, package_name.IndexOf("'"));
-
-            if (package_name.Length > 0)
-            {
-                List<string> colNames = new List<string>{
-                        "package_name"
-                };
-
-                List<string> col_vals = new List<string> {
-                        "'" + package_name + "'"
-                };
-
-                databaseConnector.UpdateSQL("A_FILE", colNames, col_vals, "FILE_ID=" + file.FILE_ID);
-            }
-        }
-
-        /// <summary>
-        /// Reads permissions
-        /// </summary>
-        private void ExtractPermissions()
-        {
-            string permissionText = string.Join("", t1.cmd("cd " + Info.TOOLS_PATH + " && aapt.exe dump permissions " + file.FILE_PATH + ""));
-
-            PermissionParser pp = new PermissionParser();
-            string json = pp.ParsePermissionJson(permissionText, file.PACKAGE_NAME);
-
-            if (json.Length > 0)
-            {
-                databaseConnector.DeleteSQL("DELETE FROM R_STATIC WHERE FILE_ID = " + file.FILE_ID);
-
-                List<string> colNames = new List<string>{
-                    "file_id",
-                    "permission_json"
-                };
-
-                List<string> col_vals = new List<string> {
-                    "'" + file.FILE_ID + "'",
-                    "'" + json + "'"
-                };
-
-                databaseConnector.InsertSQL("R_STATIC", colNames, col_vals);
-            }
-
-            string permissions = json.Replace(System.Environment.NewLine, "").Replace(" ", "").Replace("\".", "\"").Replace("N]}", "\"]}").Replace("t]}", "\"]}");
-            permissions = Regex.Replace(permissions, @"\t|\n|\r", "");
-
-            if (IsValidJson(permissions))
-            {
-                APK app = JsonConvert.DeserializeObject<APK>(permissions);
-
-                foreach (string permission in app.permissions)
-                {
-                    DataTable dt2 = databaseConnector.SelectSQL("SELECT ID FROM A_PERMISSION WHERE VALUE LIKE '" + permission + "'", "A_PERMISSION");
-                    int permission_id = (dt2.Rows.Count > 0 ? Convert.ToInt32(dt2.Rows[0]["ID"]) : -1);
-
-                    List<string> col_names = new List<string>{
-                            "FILE_ID",
-                            "PERMISSION",
-                            "PERMISSION_ID"
-                        };
-
-                    List<string> col_vals = new List<string> {
-                            "'" + file.FILE_ID + "'",
-                            "'" + permission + "'",
-                            "'" + permission_id + "'"
-                        };
-
-                    databaseConnector.DeleteSQL("DELETE FROM R_PERMISSIONS WHERE FILE_ID = " + file.FILE_ID);
-
-                    databaseConnector.InsertSQL("R_PERMISSIONS", col_names, col_vals);
-                }
-            }
-        }
-        private bool IsValidJson(string strInput)
-        {
-            strInput = strInput.Replace("[\",", "[");
-            strInput = strInput.Trim();
-            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
-                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
-            {
-                try
-                {
-                    var obj = Newtonsoft.Json.Linq.JToken.Parse(strInput);
-                    return true;
-                }
-                catch
-                {
-                    //Exception in parsing json
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Reads intents
-        /// </summary>
-        private void ExtractIntent()
-        {
-            string intentFilter = string.Join("", t1.cmd("cd " + Info.TOOLS_PATH + " && aapt.exe dump xmltree " + file.FILE_PATH + " AndroidManifest.xml"));
-            string json = "";
-
-            if (intentFilter.IndexOf("intent-filter") > -1)
-            {
-                IntentParser ip = new IntentParser();
-                json = ip.ParseIntentJson(intentFilter);
-
-                if (json.Length > 0)
-                {
-                    List<string> colNames = new List<string>{
-                        "FILE_ID",
-                        "INTENT_FILTER"
-                    };
-
-                    List<string> colVals = new List<string> {
-                        "'" + file.FILE_ID + "'",
-                        "'" + json + "'"
-                    };
-                    databaseConnector.InsertSQL("R_STATIC", colNames, colVals);
-                }
-            }
-
-            var cleanJson = json.Replace("$", "").Replace(":\"", "\",\"").Replace(System.Environment.NewLine, "");
-            if (IsValidJson(cleanJson))
-            {
-
-                APK app = JsonConvert.DeserializeObject<APK>(cleanJson);
-
-                databaseConnector.DeleteSQL("DELETE FROM R_INTENT WHERE FILE_ID = " + file.FILE_ID);
-                foreach (string intent in app.intent)
-                {
-                    List<string> col_names = new List<string>{
-                            "FILE_ID",
-                            "INTENT",
-                        };
-
-                    List<string> col_vals = new List<string> {
-                            "'" + file.FILE_ID + "'",
-                            "'" + intent + "'",
-                        };
-
-                    databaseConnector.InsertSQL("R_INTENT", col_names, col_vals);
-                }
-            }
-        }
-
-        private void Decompile()
-        {
-            t1.cmd("java -jar " + Info.TOOLS_PATH + "\\apktool.jar d -o " + Info.PROJECT_PATH + "\\files\\decoded\\" + file.ORIGINAL_FILE_NAME + " -f " + file.FILE_PATH);
-        }
-
-        /// <summary>
-        /// Get API Calls
-        /// </summary>
-        private void GetApiCalls()
-        {
-            //not implemented in this version
-        }
-
-        /// <summary>
-        /// Initiates dynamic analysis with Frida
-        /// </summary>
-        private void ExtractDynamicData()
-        {
-            //init file_id to DB
-            List<string> colNames = new List<string>{
-                    "FILE_ID"
-            };
-
-            List<string> colVals = new List<string> {
-                    "'" + file.FILE_ID + "'"
-            };
-
-            //databaseConnector.InsertSQL("A_TRAIN_DI", colNames, colVals);
-
-            string data = "";
-
-            File fileNew = new File(file.FILE_ID);
-
-            if (string.IsNullOrEmpty(fileNew.PACKAGE_NAME))
-                return;
-
-            #region ParallelTasks
-            Parallel.Invoke (
-                () =>
-                {
-                    data += t1.cmd("cd C:\\Users\\billkoul\\Downloads\\AndroidLab\\frida && frida -U -l " + Info.SCRIPT + " " + fileNew.PACKAGE_NAME, 60000);
-                },
-                () =>
-                {
-                    t1.cmd("cd C:\\Users\\billkoul\\Downloads\\AndroidLab\\platform-tools && adb.exe shell \"monkey -p " + fileNew.PACKAGE_NAME + " --pct-trackball 0 --pct-syskeys 0 --pct-nav 0 --pct-majornav 0 --ignore-crashes -v " + Info.EVENTS + "\"", 5000);
-                },
-                () =>
-                {
-                    data += t1.cmd("cd C:\\Users\\billkoul\\Downloads\\AndroidLab\\frida && frida -U -l " + Info.SCRIPT + " " + fileNew.PACKAGE_NAME,60000);
-                }
-            );
-            //scan.js => traffic
-            //intent4.js => intent
-            #endregion
-
-            System.Threading.Thread.Sleep(5000);
-
-            if (!string.IsNullOrEmpty(data))
-            {
-                colNames = new List<string>{
-                    "FILE_ID"
-                };
-                if (!string.IsNullOrEmpty(data)) colNames.Add("TRAFFIC");
-
-                colVals = new List<string> {
-                    "'" + fileNew.FILE_ID + "'"
-                };
-                if (!string.IsNullOrEmpty(data)) colVals.Add("'" + (data.IndexOf("Attaching") != -1 ? data.Substring(data.IndexOf("Attaching")) : data) + "'");
-
-                databaseConnector.UpdateSQL("A_TRAIN_DI", colNames, colVals, "FILE_ID="+ fileNew.FILE_ID);
-
-                return;
-            }
-        }
     }
 }
